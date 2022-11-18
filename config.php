dba88b23a8d978a9888f7d73a22ff1c54f6d725b
lubyelcuritiba <lubyel.lopes@gmail.com>
2022-11-18 17:45:18 -0300

    Effect

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+    define('INCLUDE_PATH','http://localhost/Social Media Icon Effect/');//url amigável
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include('config.php'); ?>
+<html>
+
+<head>
+    <title>Social Media Icon Effect</title>
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>fonts/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
+    <link href="<?php echo INCLUDE_PATH; ?>style.css" rel="stylesheet" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras-chave, do, meu, site">
+    <meta name="description" content="Descrição do meu website">
+    <meta name="author" content="Ariel Lopes">
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <ul>
+        <li><a href="#"><i class="fa-brands fa-instagram instagram"></i></a></li>
+        <li><a href="#"><i class="fa-brands fa-whatsapp whatsapp"></i></a></li>
+        <li><a href="#"><i class="fa-brands fa-twitter twitter"></i></a></li>
+    </ul>
+</body>
+
+</html>
